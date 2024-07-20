c21fca4a565b89f017b3f6474b3c1d6433a47243
gior988 <giordanercole@gmail.com>
2024-07-20 14:59:57 +0700

    hi

---
+++ main.py
@@ -0,0 +1,21 @@
+import turtle
+screen=turtle.Screen()
+
+pacman=turtle.Turtle()
+pacman.shape("circle")
+pacman.shapesize(2,2)
+pacman.penup()
+pacman_direction="up"
+pacman.setheading(90)
+pacman.goto(-300,250)
+
+
+def pacman_go_up():
+    pacman.setheading(90)
+    pacman_direction="up"
+
+screen.onkeypress(pacman_go_up,"Up")
+screen.listen()
+
+while True:
+    screen.update()
